272b9cea3227735e78bc598149fd8a7e5875ef55
iangithua <iangithua6@gmail.com>
2023-04-17 12:42:38 +0300

    Created compare and view pages

---
+++ src/main/java/com/eatta/currencyexchange/controller/CurrencyController.java
@@ -0,0 +1,24 @@
+package com.eatta.currencyexchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CurrencyController {
+	
+	@GetMapping("/")
+	String getCurrency(Model model)
+	{
+		model.addAttribute("something","something from controller");
+		return "currency";
+	}
+	
+	@GetMapping("/compare")
+	String getConvertion(Model model)
+	{
+		model.addAttribute("something","something from controller");
+		return "convert";
+	}
+
+}
